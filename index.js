d5f0499c849f20e99ec78bffbd654b52b4d8af92
sidfubo1 <sidfubo1@gmail.com>
2018-07-16 23:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 let currentUser='Sid Shah';
 let welcomeMessage='Welcome to Flatbook, ' + currentUser + '!';
 let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+let shortGreeting='Welcome ,'
