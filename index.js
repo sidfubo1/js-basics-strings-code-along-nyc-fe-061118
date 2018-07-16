3ad3470f0f5ce8d4fa002d3cb0e3b983c44c0478
sidfubo1 <sidfubo1@gmail.com>
2018-07-16 23:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser='Sid Shah';
 let welcomeMessage='Welcome to Flatbook, ' + currentUser + '!';
-let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' + string.
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' + str.toUpperCast
